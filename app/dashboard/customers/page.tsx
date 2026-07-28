7652c8a75dee7a97ab3237c9d14ab10691a9648a
Tinnnooo <tinnofabregas01@gmail.com>
2026-07-28 03:38:06 +0000

    feat: add metadata to dashboard pages and implement customer pagination

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,35 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { fetchCustomersPages } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import { lusitana } from "@/app/ui/fonts";
+import Pagination from "@/app/ui/invoices/pagination";
+import Search from "@/app/ui/search";
+import { TableRowSkeleton } from "@/app/ui/skeletons";
+import type { Metadata } from "next";
+import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchCustomersPages(query);
+
+  return (
+    <div className="w-full">
+      <Suspense key={query + currentPage} fallback={<TableRowSkeleton />}>
+        <CustomersTable query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
